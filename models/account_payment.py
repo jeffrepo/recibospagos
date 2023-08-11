cd23c659b844e226e9bea35419a180e2a496c754
rocketgithub <rodrigo@aquih.com>
2023-08-11 14:04:35 -0600

    Version 16.0

--- models/account_payment.py
+++ models/account_payment.py
@@ -6,45 +6,46 @@ class AccountPayment(models.Model):
     _inherit = "account.payment"
 
     pago_origen_id = fields.Many2one('recibo.pago',string="Pago origen")
-    # empleado_id = fields.Many2one('hr.employee','Empleado')
     pago_liquidacion_ids = fields.One2many('recibo.pago','pago_id',string="Pagos")
 
-    @api.onchange('pago_liquidacion_ids')
-    def onchange_pago_liquidacion_ids(self):
-        total = 0
-        if self.pago_liquidacion_ids:
-            if version_info[0] == 14:
-                cuenta_destino_id = self.pago_liquidacion_ids[0].diario_id.default_account_id
-                self.destination_account_id = cuenta_destino_id.id
-            for pago in self.pago_liquidacion_ids:
-                if pago.linea_pago_ids:
-                    for linea in pago.linea_pago_ids:
-                        total += linea.pago
-        self.amount = total
+    # empleado_id = fields.Many2one('hr.employee','Empleado')
 
-    if version_info[0] == 13:
-        def post(self):
-            res = super(AccountPayment, self).post()
-            for rec in self:
-                if rec.pago_liquidacion_ids:
-                    for pago in rec.pago_liquidacion_ids:
-                        pago.write({'pago_origen_id': rec.id})
-            return True
-    else:
-        def action_post(self):
-            res = super(AccountPayment, self).action_post()
-            for rec in self:
-                if rec.pago_liquidacion_ids:
-                    for pago in rec.pago_liquidacion_ids:
-                        pago.write({'pago_origen_id': rec.id})
-            return True
+    # @api.onchange('pago_liquidacion_ids')
+    # def onchange_pago_liquidacion_ids(self):
+    #     total = 0
+    #     if self.pago_liquidacion_ids:
+    #         if version_info[0] == 14:
+    #             cuenta_destino_id = self.pago_liquidacion_ids[0].diario_id.default_account_id
+    #             self.destination_account_id = cuenta_destino_id.id
+    #         for pago in self.pago_liquidacion_ids:
+    #             if pago.linea_pago_ids:
+    #                 for linea in pago.linea_pago_ids:
+    #                     total += linea.pago
+    #     self.amount = total
 
-        @api.depends('partner_id', 'destination_account_id', 'journal_id')
-        def _compute_is_internal_transfer(self):
-            res = super(AccountPayment, self)._compute_is_internal_transfer()
-            for payment in self:
-                if payment.pago_liquidacion_ids:
-                    payment.is_internal_transfer = True
-                    cuenta_destino_id = payment.pago_liquidacion_ids[0].diario_id.default_account_id
-                    payment.destination_account_id = cuenta_destino_id.id
-            return res
+    # if version_info[0] == 13:
+    #     def post(self):
+    #         res = super(AccountPayment, self).post()
+    #         for rec in self:
+    #             if rec.pago_liquidacion_ids:
+    #                 for pago in rec.pago_liquidacion_ids:
+    #                     pago.write({'pago_origen_id': rec.id})
+    #         return True
+    # else:
+    #     def action_post(self):
+    #         res = super(AccountPayment, self).action_post()
+    #         for rec in self:
+    #             if rec.pago_liquidacion_ids:
+    #                 for pago in rec.pago_liquidacion_ids:
+    #                     pago.write({'pago_origen_id': rec.id})
+    #         return True
+
+    #     @api.depends('partner_id', 'destination_account_id', 'journal_id')
+    #     def _compute_is_internal_transfer(self):
+    #         res = super(AccountPayment, self)._compute_is_internal_transfer()
+    #         for payment in self:
+    #             if payment.pago_liquidacion_ids:
+    #                 payment.is_internal_transfer = True
+    #                 cuenta_destino_id = payment.pago_liquidacion_ids[0].diario_id.default_account_id
+    #                 payment.destination_account_id = cuenta_destino_id.id
+    #         return res
